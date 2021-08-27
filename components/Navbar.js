0c255f1c84ffe6a311bfb08052730ba3aaae05cb
Anthony Sim <anthonyysim@gmail.com>
2021-08-26 19:15:00 -0700

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useState } from 'react';
-import styles from '../styles/Navbar.module.css'
+
 
 export default function Navbar() {
   const [active, setActive] = useState(false);
@@ -10,13 +10,12 @@ export default function Navbar() {
   };
 
   return (
-
     <div>
       <nav className='text flex items-center flex-wrap bg-gray-900 p-3'>
         <Link href='/'>
-          <a className='inline-flex items-center pl-12 p-2 mr-4 '>
+          <a className='inline-flex items-center pl-12 p-2 mr-4'>
             <span className='text-xl text-white font-bold uppercase tracking-wide'>
-              Anthony Sim
+              Anthony Sim | DEV
             </span>
           </a>
         </Link>
@@ -46,22 +45,22 @@ export default function Navbar() {
         >
           <div className='pr-12 lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
             <Link href='/'>
-              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center  hover:text-white '>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-50 font-bold items-center justify-center  hover:text-blue-300 uppercase'>
                 Home
               </a>
             </Link>
             <Link href='/'>
-              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center  hover:text-white'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-100 font-bold items-center justify-center  hover:text-blue-300 uppercase'>
                 Portfolio
               </a>
             </Link>
             <Link href='/'>
-              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:text-white'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-200 font-bold items-center justify-center hover:text-blue-300 uppercase'>
                 About
               </a>
             </Link>
             <Link href='/'>
-              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-300 font-bold items-center justify-center hover:text-blue-400'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-300 font-bold items-center justify-center hover:text-blue-300 uppercase'>
                 Contact
               </a>
             </Link>
