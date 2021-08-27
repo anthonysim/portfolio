3cc8e88aaf65c129a4c9ceb78210ff278b020f43
Anthony Sim <anthonyysim@gmail.com>
2021-08-26 17:44:11 -0700

    initial

---
+++ components/Navbar.js
@@ -0,0 +1,73 @@
+import Link from 'next/link';
+import { useState } from 'react';
+import styles from '../styles/Navbar.module.css'
+
+export default function Navbar() {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
+  return (
+
+    <div>
+      <nav className='text flex items-center flex-wrap bg-gray-900 p-3'>
+        <Link href='/'>
+          <a className='inline-flex items-center pl-12 p-2 mr-4 '>
+            <span className='text-xl text-white font-bold uppercase tracking-wide'>
+              Anthony Sim
+            </span>
+          </a>
+        </Link>
+        <button
+          className=' inline-flex p-3 hover:bg-blue-400 rounded lg:hidden text-white ml-auto hover:text-white outline-none'
+          onClick={handleClick}
+        >
+          <svg
+            className='w-6 h-6'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M4 6h16M4 12h16M4 18h16'
+            />
+          </svg>
+        </button>
+        {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+        <div
+          className={`${active ? '' : 'hidden'
+            }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        >
+          <div className='pr-12 lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center  hover:text-white '>
+                Home
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center  hover:text-white'>
+                Portfolio
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:text-white'>
+                About
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-300 font-bold items-center justify-center hover:text-blue-400'>
+                Contact
+              </a>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
