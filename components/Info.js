bdb01e3fc4e0064a57f58666ef418883ea1b8cfa
Anthony Sim <anthonyysim@gmail.com>
2021-09-03 11:05:47 -0700

    fixed weird responsive issues

--- components/Info.js
+++ components/Info.js
@@ -6,22 +6,15 @@ export default function Info() {
   return (
     <div className="grid md:grid-cols-2 sm:grid-cols-1 place-items-center">
       <Fade>
-        <div >
+        <div className="md:ml-40">
           <Image className="rounded-full border border-gray-100 shadow-sm" src="/me.png" width={200}
             height={200} alt="me" priority />
         </div>
-        <div>
-          <p>Hello, my name is Anthony Sim, I am a Fullstack</p>
-          <p>Software Engineer from Los Angeles, CA with a</p>
-          <p> background in Accounting, Auditing and Finance.</p>
+        <div className="text-center md:mr-40">
+          <p>Hello, my name is Anthony Sim, I am a Fullstack Software Engineer from Los Angeles, CA with a background in Accounting, Auditing and Finance.</p>
           <br />
-          <p>Besides tech, I enjoy coffee, tacos, traveling, </p>
-          <p>investing in crypto, different perspectives and</p>
-          <p>reading memoirs.</p>
+          <p>Besides tech, I enjoy coffee, tacos, traveling, investing in crypto, different perspectives and reading memoirs.</p>
         </div>
-        <br />
-        <br />
-        <br />
         <br />
         <br />
       </Fade>
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
@@ -7,6 +8,9 @@ import ScrollToTop from '../components/ScrollToTop';
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+      </Head>
       <ThemeProvider attribute="class">
         <Navbar />
         <Component {...pageProps} />
