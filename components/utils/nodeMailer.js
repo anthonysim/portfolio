da331871d4422327bcaf83c290d278255f4d06c2
Anthony Sim <anthonyysim@gmail.com>
2022-04-05 11:43:59 -0700

    removed nodeMailer kept failing

--- components/utils/nodeMailer.js
+++ components/utils/nodeMailer.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
-import validator from 'validator';
+// import axios from 'axios';
+// import validator from 'validator';
 
-export default function nodeMailer(data, setThankyou) {
-  if (data.firstName === ''
-    || data.lastName === ''
-    || data.email === ''
-    || data.firstName === ''
-    || !validator.isEmail(data.email)) {
-    alert('Please fill out all fields.');
-  } else {
-    axios.post('/api/contact', data)
-      .then(res => {
-        console.log(res);
-        setThankyou(true);
-        setTimeout(() => setThankyou(false), 3750);
-      })
-      .catch(err => console.error(err));
-  }
-};
+// export default function nodeMailer(data, setThankyou) {
+//   if (data.firstName === ''
+//     || data.lastName === ''
+//     || data.email === ''
+//     || data.firstName === ''
+//     || !validator.isEmail(data.email)) {
+//     alert('Please fill out all fields.');
+//   } else {
+//     axios.post('/api/contact', data)
+//       .then(res => {
+//         console.log(res);
+//         setThankyou(true);
+//         setTimeout(() => setThankyou(false), 3750);
+//       })
+//       .catch(err => console.error(err));
+//   }
+// };
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -3,7 +3,8 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Fade } from "react-awesome-reveal";
-import nodeMailer from '../../components/utils/nodeMailer.js';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
 import styles from '../../styles/Home.module.css';
 
 export default function Contact() {
@@ -12,23 +13,6 @@ export default function Contact() {
   const lastNameInput = useRef(null);
   const emailInput = useRef(null);
   const messageInput = useRef(null);
-
-  const onButtonClick = (e) => {
-    e.preventDefault();
-    const data = {
-      firstName: firstNameInput.current.value,
-      lastName: lastNameInput.current.value,
-      email: emailInput.current.value,
-      message: messageInput.current.value,
-    }
-
-    nodeMailer(data, setThankyou);
-
-    firstNameInput.current.value = '';
-    lastNameInput.current.value = '';
-    emailInput.current.value = '';
-    messageInput.current.value = '';
-  };
 
   return (
     <div className={styles.container}>
@@ -39,65 +23,25 @@ export default function Contact() {
           <link rel="icon" href="/AS.png" />
         </Head>
         <main className={styles.main}>
-          <div className="max-w-screen-md mx-auto p-5">
-            <div className="text-center mb-16">
-              {/* GET IN TOUCH / THANK YOU  MESSAGE*/}
-              {!thankyou
-                ? <h3 className="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight text-gray-900 dark:text-white">Get In <span className="text-blue-600">Touch</span>
-                </h3>
-                : <h3 className="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight text-gray-900">Thank You for Your <span className="text-blue-600">Message!</span>
-                </h3>}
-            </div>
 
-            {/* FORM */}
-            {!thankyou && <form className="w-full">
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                  <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
-                    First Name
-                  </label>
-                  <input ref={firstNameInput} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="firstname" autoComplete="grid-first-name" required />
-                </div>
-                <div className="w-full md:w-1/2 px-3">
-                  <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name"  >
-                    Last Name
-                  </label>
-                  <input ref={lastNameInput} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="lastname" autoComplete="grid-last-name" required />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3">
-                  <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
-                    Email Address
-                  </label>
-                  <input ref={emailInput} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="********@*****.**" required />
-                </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3">
-                  <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
-                    Your Message
-                  </label>
-                  <textarea type="text" ref={messageInput} rows="10" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" required />
-                </div>
-                <input type="hidden" name="_subject" value="Anthony Sim | DEV"></input>
-                <input type="hidden" name="_next" value="https://anthonysim.dev/thanks"></input>
-                <div className="flex justify-between w-full px-3">
-                  <div className="md:flex md:items-center">
-                    <label className="block text-gray-500 font-bold">
-                      <span className="text-sm">
-                        Send me a message!
-                      </span>
-                    </label>
-                  </div>
-                  <button onClick={onButtonClick} className="shadow bg-blue-600 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded" type="submit">
-                    Send Message
-                  </button>
-                </div>
-              </div>
-            </form>}
+          {/* Get In Touch */}
+          <h3 className="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight text-gray-900 dark:text-white">Get In <span className="text-blue-600">Touch</span>
+          </h3>
+          <br />
+
+          {/* Email */}
+          <div className="text-black text-3xl space-x-8 justify-center pt-4 dark:text-blue-600">
+            <a href="mailto: anthonyysim@gmail.com">
+              <span className="">email: </span> <AiOutlineMail className="inline-block dark:text-white" />
+            </a>
           </div>
         </main>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </Fade>
     </div>
   )
